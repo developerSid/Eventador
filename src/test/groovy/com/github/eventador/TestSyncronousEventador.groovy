b96178e13f27bb39acd0e88aebd581189d3100df
Developer Sid <developer.sid@gmail.com>
2012-11-01 21:15:57 -0500

    Unit test work for testing the Syncronous event sourcing

--- src/test/groovy/com/github/eventador/TestSyncronousEventador.groovy
+++ src/test/groovy/com/github/eventador/TestSyncronousEventador.groovy
@@ -7,17 +7,27 @@ import com.github.eventador.impl.SyncronousEventador
 
 class TestSyncronousEventador extends Specification
 {
-   private Eventador eventador;
-   def sourcing
-   
-   def setup()
-   {
-      sourcing=Mock(SourcingWriter);
-      eventador=new SyncronousEventador(sourcing)
-   }
-   
    def "test eventador with sourcing no listeners" ()
    {
-      
+      setup:
+         def event=Mock(Serializable)
+         def sourcing=Mock(SourcingWriter)
+         def eventador=new SyncronousEventador(sourcing)
+      when:
+         eventador.publish(event)
+      then:
+         notThrown Exception
+         1 * sourcing.source(_)
+   }
+   def "test eventador with no sourcing single listener" ()
+   {
+      setup:
+         def listener=Spy(EventObjectTestListener, constructorArgs:[])
+         def event=new EventObjectTest("64E2CA86-42BD-47AF-8516-659ED97A0AFB")
+         def eventador=new SyncronousEventador()
+      when:
+         eventador.publish(event)
+      then:
+         1 * listener.process(_)
    }
 }
